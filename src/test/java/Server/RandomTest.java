3399adb52ee5f509d9e9841bb0e4a0fb104ae31c
Sachin Bhargava <sachin20@uw.edu>
2020-05-07 01:05:26 -0700

    playing with ci/cd

---
+++ src/test/java/Server/RandomTest.java
@@ -0,0 +1,15 @@
+package Server;
+
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RandomTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(5, 2 + 2 + 1);
+    }
+
+}
